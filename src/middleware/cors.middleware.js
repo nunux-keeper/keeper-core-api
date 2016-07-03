a50625b0c80133522c02717ff9de378e82f37c8c
Nicolas Carlier <n.carlier@nunux.org>
2016-07-03 22:59:46 +0200

    fix(cors): Use dynamic origin.

--- src/middleware/cors.middleware.js
+++ src/middleware/cors.middleware.js
@@ -5,7 +5,8 @@
  */
 module.exports = function () {
   return function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
+    // TODO Use whitelisted origin
+    res.header('Access-Control-Allow-Origin', req.headers.origin)
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
     res.header('Access-Control-Allow-Credentials', 'true')
