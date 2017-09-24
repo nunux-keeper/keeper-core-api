c81989aa057b0822ae2ebf2e89c7c2f1cbe266a4
Nicolas Carlier <n.carlier@nunux.org>
2017-09-25 00:08:31 +0200

    fix(export): make SSE compatible with NGINX

--- src/controller/exports.ctrl.js
+++ src/controller/exports.ctrl.js
@@ -16,7 +16,8 @@ module.exports = {
     res.writeHead(200, {
       'Content-Type': 'text/event-stream',
       'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive'
+      'Connection': 'keep-alive',
+      'X-Accel-Buffering': 'no'
     })
     res.write('\n')
 
