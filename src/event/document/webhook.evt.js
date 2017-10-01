318e934f372fd8f8179552e0fda16e4580646635
Nicolas Carlier <n.carlier@nunux.org>
2017-10-01 10:13:32 +0200

    feat(webhook): add Webhook event handler

---
+++ src/event/document/webhook.evt.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const _ = require('lodash')
+const request = require('request')
+const crypto = require('crypto')
+const webhookDao = require('../../dao').webhook
+const logger = require('../../helper').logger
+const urlConfig = require('../../helper').urlConfig
+
+/**
+ * Trigger for a webhook
+ * @param {String} event Event name
+ * @param {Object} webhook the webhook
+ * @param {Object} doc the documenet to send as payload
+ */
+const triggerWebhook = function (event, webhook, doc) {
+  const _doc = _.pick(doc, ['id', 'title', 'labels', 'origin', 'date'])
+  const payload = {
+    action: `${event}_document`,
+    issue: {
+      url: urlConfig.baseUrl,
+      date: new Date()
+    },
+    document: _doc
+  }
+  let headers = {
+    'User-Agent': 'Keeper-Hookshot'
+  }
+  if (webhook.secret) {
+    headers['X-Hub-Signature'] = crypto.createHmac('sha1', webhook.secret)
+      .update(JSON.stringify(payload)).digest('hex')
+  }
+
+  logger.debug(`Sending payload to Webhook ${webhook.url} ...`, payload)
+  request({
+    url: webhook.url,
+    method: 'POST',
+    json: payload,
+    headers
+  }, function (err, res, body) {
+    if (err || res.statusCode > 299) {
+      logger.error('Unable to send payload the Webhook', err || body)
+    } else {
+      logger.debug(`Payload sent to Webhook ${webhook.url}`, payload)
+    }
+  })
+}
+
+/**
+ * Trigger for all webhooks matching an document and en event
+ * @param {Object} event Event to filter webhooks
+ * @param {Object} doc   Document to filter webhooks and to send as payload
+ */
+const triggerWebhooks = function (event, doc) {
+  const query = {
+    owner: doc.owner,
+    active: true,
+    events: event
+  }
+  if (doc.labels) {
+    query.labels = doc.labels
+  }
+
+  webhookDao.find(query, {order: 'asc', from: 0, size: 100})
+    .then(
+      results => results.forEach(webhook => triggerWebhook(event, webhook, doc)),
+      err => logger.error('Error while processing webhook', err)
+    )
+}
+
+/**
+ * Document event handler.
+ */
+module.exports = function (documentEventHandler) {
+  documentEventHandler.on('create', doc => triggerWebhooks('create', doc))
+  documentEventHandler.on('update', doc => triggerWebhooks('update', doc))
+  documentEventHandler.on('remove', doc => triggerWebhooks('remove', doc))
+}
